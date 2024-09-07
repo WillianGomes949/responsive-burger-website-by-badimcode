552651556914da4a9ebd10fdbf383dc46f7dd8f2
WillianGomes949 <williangomes949@gmail.com>
2024-09-07 14:53:21 -0300

    hero e header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,42 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
+/*Menu Show*/
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+/*Menu hidden*/
+
+if(navClose){
+    navClose.addEventListener('click', () => {
+            navMenu.classList.remove('show-menu');
+    });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll('.nav__link')
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    //quando clicar em cada nav__link, removemos o show-menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(e => e.addEventListener('click', linkAction))
 
 /*=============== ADD SHADOW HEADER ===============*/
 
+const shadowHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50  ? header.classList.add('shadow-header')
+                        : header.classList.remove('shadow-header');         
+}
+window.addEventListener('scroll', shadowHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
